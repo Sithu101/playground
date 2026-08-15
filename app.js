be607c24571c6de92a2112d60a1d4b75f9120b33
Sithu101 <Sithu101@users.noreply.github.com>
2026-08-15 14:37:16 +0630

    jornal app

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-const readline = require('readline');
+const readline = require('readline')
+const fs = require('fs')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+})
 
-const targetNum = Math.floor(Math.random() * 10) + 1;
+const today = new Date().toISOString().split('T')[0]
+const flineName = `journal-${today}.txt`;
 
-console.log('Guess a number between 1 and 10:');
-console.log('i picked a number between 1 and 10. Can you guess it?');
+console.log(`welcome to journal![Date:${today}]`);
 
-function askQuestion() {
-    rl.question('Enter your guess: ', (answer) => {
-        const guess = parseInt(answer);
-        if (guess === targetNum) {
-            console.log('Congratulations! You guessed the correct number!');
-            rl.close();
-        } else if (guess < targetNum) {
-            console.log('Too low! Try again.');
-            askQuestion();
-        } else if (guess > targetNum) {
-            console.log('Too high! try again');
-            askQuestion()
-        } else {
-            console.log('invlaid num')
-            askQuestion()
-        }
-    });
-}
+rl.question('Write your journal \n>', (entry) => {
+    if (entry.trim() === '') {
+        console.log('cant be empty');
+        rl.close();
+        return;
+    }
 
-askQuestion();
+    const logTime = new Date().toLocaleDateString();
+    const contentTosave = `n[${logTime}] ${entry}\n------------------`;
+
+    fs.appendFileSync(flineName, contentTosave);
+
+    console.log(`saved to"${flineName}"!`);
+    rl.close();
+})
